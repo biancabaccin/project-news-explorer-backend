e7bf7df2c0288b4a6fb928242151cd0ef9199ab5
BiancaBaccin <bianca.baccin@gmail.com>
2026-05-11 20:24:40 -0300

    Cria Schemas User e Article

---
+++ models/article.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  fonte: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => /^https?:\/\/.+/i.test(v),
+      message: "link must be a valid URL",
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => /^https?:\/\/.+/i.test(v),
+      message: "image must be a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model("Article", articleSchema);
